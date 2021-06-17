1525465c409722efc44556b301de0cc9d0413ad3
victor <victor.alexandre@atlantasistemas.com>
2021-06-16 21:00:31 -0300

    Exemplos de input text

--- app/src/main/java/com/inux/textinputedittextapp/MainActivity.kt
+++ app/src/main/java/com/inux/textinputedittextapp/MainActivity.kt
@@ -2,10 +2,30 @@ package com.inux.textinputedittextapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.widget.Toast
+import androidx.core.widget.doOnTextChanged
+import com.inux.textinputedittextapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(LayoutInflater.from(this))
+        setContentView(binding.root)
+
+        binding.telefoneTextInputLayout.setEndIconOnClickListener {
+            Toast.makeText(this, "Clicado!", Toast.LENGTH_LONG).show()
+        }
+
+        binding.telefoneEdt.doOnTextChanged { text, start, before, count ->
+            if (text!!.length > 11) {
+                binding.telefoneTextInputLayout.error = "Número de telefone incorreto."
+            }else{
+                binding.telefoneTextInputLayout.error = null
+            }
+        }
     }
 }
